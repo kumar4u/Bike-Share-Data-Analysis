fdf6e59f5e0173860f9694911d1d87f7619f2cfe
Muthukumar Palavesam <pmkumar2@gmail.com>
2019-04-15 18:20:41 -0700

    Added docstring for the get_filters function in bikeshare.py file

--- bikeshare.py
+++ bikeshare.py
@@ -14,6 +14,14 @@ months = ['january', 'february', 'march', 'april', 'may', 'june']
 days = ['sunday', 'monday', 'tuesday', 'wednesday','thursday', 'friday', 'saturday']
 
 def get_filters():
+    """
+    Asks user to specify a city, month, and day to analyze.
+
+    Returns:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    """
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
